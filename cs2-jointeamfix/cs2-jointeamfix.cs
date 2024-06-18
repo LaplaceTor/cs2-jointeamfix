0eef9b85227b49e27c21e508cf4c1e01b1142d74
Laplace <405420687@qq.com>
2024-06-19 03:22:43 +0800

    update

--- cs2-jointeamfix/cs2-jointeamfix.cs
+++ cs2-jointeamfix/cs2-jointeamfix.cs
@@ -1,4 +1,4 @@
-using CounterStrikeSharp.API;
+﻿using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Modules.Utils;
 
@@ -8,42 +8,84 @@ public class JoinTeamFix : BasePlugin
 {
     public override string ModuleName => "cs2-jointeamfix";
     public override string ModuleAuthor => "Lapl";
-    public override string ModuleVersion => "3.0.1";
-
+    public override string ModuleVersion => "5.1";
     private string[] TeamValue = new string[3];
-
     private List<string[]> TeamHistory = new();
-
     private Random random = new();
+    private List<CBaseEntity> spawnentityct = new();
+    private List<CBaseEntity> spawnentityt = new();
+    private List<Vector> spawnoriginct = new();
+    private List<Vector> spawnorigint = new();
+    private List<ulong> changeteamcalm = new();
 
     public override void Load(bool hotLoad)
     {
+        RegisterListener<Listeners.OnMapStart>((onmapstart) =>
+        {
+            spawnentityct.Clear();
+            spawnentityt.Clear();
+            spawnoriginct.Clear();
+            spawnorigint.Clear();
+            Server.NextFrame(()=>
+            {
+                spawnentityct = Utilities.FindAllEntitiesByDesignerName<CBaseEntity>("info_player_counterterrorist").ToList();
+                if(spawnentityct.Count != 0)
+                {
+                    foreach(var entity in spawnentityct)
+                    {
+                        entity.AbsOrigin!.Z += 16f;
+                        spawnoriginct.Add(entity.AbsOrigin!);
+                    }
+                }
+                spawnentityt = Utilities.FindAllEntitiesByDesignerName<CBaseEntity>("info_player_terrorist").ToList();
+                if(spawnentityt.Count != 0)
+                {
+                    foreach(var entity in spawnentityt)
+                    {
+                        entity.AbsOrigin!.Z += 16f;
+                        spawnorigint.Add(entity.AbsOrigin!);
+                    }
+                }
+            });
+        });
         RegisterEventHandler<EventRoundStart>((@event, info) =>
         {
             TeamHistory.Clear();
+            changeteamcalm.Clear();
             var playerlist = Utilities.GetPlayers().Where((x) => x.TeamNum > 1).ToList();
             TeamBalanceCheck(playerlist);
             foreach (var player in playerlist)
                 if (!player.PawnIsAlive)
-                    TryJoinTeam(player);
+                    JoinTeam(player);
             return HookResult.Continue;
         });
         RegisterEventHandler<EventPlayerTeam>((@event, info) =>
         {
-            if (!@event.Isbot && !@event.Disconnect)
+            if (!@event.Isbot)
             {
-                TeamValue = new string[] { @event.Userid.SteamID.ToString(), @event.Team.ToString(), @event.Oldteam.ToString() };
-                TeamHistory.Add(TeamValue);
+                if(!@event.Disconnect)
+                {
+                    TeamValue = new string[] { @event.Userid!.SteamID.ToString(), @event.Team.ToString(), @event.Oldteam.ToString() };
+                    TeamHistory.Add(TeamValue);
+                }
+                else
+                {
+                    TeamHistory = TeamHistory.Where(x => !x.Contains(@event.Userid!.SteamID.ToString())).ToList();
+                    changeteamcalm = changeteamcalm.Where(x => x != @event.Userid!.SteamID).ToList();
+                }
             }
-            else
-                TeamHistory = TeamHistory.Where(x => !x.Contains(@event.Userid.SteamID.ToString())).ToList();
             return HookResult.Continue;
         });
         RegisterEventHandler<EventJointeamFailed>((@event, info) =>
         {
-            if (TeamHistory.Any(x => x.Contains(@event.Userid.SteamID.ToString())) && @event.Userid.TeamNum < 2)
+            if (TeamHistory.Any(x => x.Contains(@event.Userid!.SteamID.ToString())))
             {
-                var playerhistory = TeamHistory.Where(x => x.Contains(@event.Userid.SteamID.ToString())).FirstOrDefault();
+                if(changeteamcalm.Contains(@event.Userid!.SteamID))
+                {
+                    @event.Userid.PrintToChat("Change team calm until the next round");
+                    return HookResult.Continue;
+                }
+                var playerhistory = TeamHistory.Where(x => x.Contains(@event.Userid.SteamID.ToString())).First();
                 if (@event.Userid.TeamNum == int.Parse(playerhistory![2]))
                     switch (int.Parse(playerhistory[1]))
                     {
@@ -51,8 +93,9 @@ public class JoinTeamFix : BasePlugin
                         case 2: @event.Userid.ChangeTeam(CsTeam.Terrorist); break;
                         case 3: @event.Userid.ChangeTeam(CsTeam.CounterTerrorist); break;
                     }
+                changeteamcalm.Add(@event.Userid!.SteamID);
             }
-            TeamHistory = TeamHistory.Where(x => !x.Contains(@event.Userid.SteamID.ToString())).ToList();
+            TeamHistory = TeamHistory.Where(x => !x.Contains(@event.Userid!.SteamID.ToString())).ToList();
             return HookResult.Continue;
         });
     }
@@ -89,27 +132,32 @@ public class JoinTeamFix : BasePlugin
             return;
     }
 
-    public void TryJoinTeam(CCSPlayerController player)
+    public void JoinTeam(CCSPlayerController player)
     {
         bool FoundSpawnplace = false;
-        QAngle? spawnangle;
         Vector? spawnorigin;
-        List<CBaseEntity> spawnentitygroup = new();
-        CBaseEntity? spawnentity = null;
+        List<Vector> spawnorigingroup = new();
         bool searchany = false;
+        Random random = new();
 
         while (!FoundSpawnplace)
         {
-            if (player.TeamNum == 2 || searchany == true)
-                spawnentitygroup = Utilities.FindAllEntitiesByDesignerName<CBaseEntity>("info_player_counterterrorist").ToList();
-            if (player.TeamNum == 3 || searchany == true)
-                spawnentitygroup = Utilities.FindAllEntitiesByDesignerName<CBaseEntity>("info_player_counterterrorist").ToList();
-            if (spawnentity != null)
+            if (player.TeamNum == 2)
             {
-                spawnentity = spawnentitygroup[random.Next(0, spawnentitygroup.Count - 1)];
-                spawnangle = spawnentity.AbsRotation;
-                spawnorigin = spawnentity.AbsOrigin;
-                player.PlayerPawn.Value!.Teleport(spawnorigin!, spawnangle!, new Vector(0, 0, 0));
+                spawnorigingroup = spawnorigint;
+                if(searchany == true)
+                    spawnorigingroup = spawnoriginct;
+            }
+            else if (player.TeamNum == 3)
+            {
+                spawnorigingroup = spawnoriginct;
+                if(searchany == true)
+                    spawnorigingroup = spawnorigint;
+            }
+            if (spawnorigingroup.Count() != 0)
+            {
+                spawnorigin = spawnorigingroup[random.Next(0,spawnorigingroup.Count-1)];
+                player.PlayerPawn.Value!.Teleport(spawnorigin!, new QAngle(0, 0, 0), new Vector(0, 0, 0));
                 player.Respawn();
                 FoundSpawnplace = true;
             }
